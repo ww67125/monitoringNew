2ac6db870d0cb8cf233b18846d652b6cc1b664e0
wangz <1014626546@qq.com>
2018-11-03 16:55:02 +0800

    job test

--- src/main/java/com/runcheck/monitoring/jpa/equipment/EquipJPA.java
+++ src/main/java/com/runcheck/monitoring/jpa/equipment/EquipJPA.java
@@ -1,7 +1,12 @@
 package com.runcheck.monitoring.jpa.equipment;
 
 import com.runcheck.monitoring.entity.equipment.EquiptmentEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface EquipJPA extends JpaRepository<EquiptmentEntity,Long> {
+public interface EquipJPA extends JpaRepository<EquiptmentEntity,Long>,JpaSpecificationExecutor<EquiptmentEntity> {
+    public Page<EquiptmentEntity> findAllByCompanyNameContainingAndEquipmentNameContaining(String Com, String eq,String no, Pageable pageable);
 }
+
--- src/test/java/com/runcheck/monitoring/MonitoringApplicationTests.java
+++ src/test/java/com/runcheck/monitoring/MonitoringApplicationTests.java
@@ -1,16 +1,44 @@
 package com.runcheck.monitoring;
 
+import com.runcheck.monitoring.entity.equipment.EquiptmentEntity;
+import com.runcheck.monitoring.jpa.equipment.EquipJPA;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MonitoringApplicationTests {
-
+    @Autowired
+    EquipJPA equipJPA;
     @Test
     public void contextLoads() {
+//        Pageable pageable=new PageRequest()
+        Page<EquiptmentEntity> pages=equipJPA.findAll(new Specification<EquiptmentEntity>() {
+                                                          @Override
+                                                          public Predicate toPredicate(Root<EquiptmentEntity> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                                                              Predicate p1 = criteriaBuilder.like(root.get("companyName").as(String.class), "%2%");
+                                                              Predicate p2 = criteriaBuilder.like(root.get("equipmentName").as(String.class), "%2%");
+                                                              Predicate p3 = criteriaBuilder.like(root.get("equipmentNo").as(String.class), "%a%");
+                                                               criteriaQuery.where(criteriaBuilder.and(criteriaBuilder.or(p1,p2),p3));
+                                                              return criteriaQuery.getGroupRestriction();
+                                                          }
+                                                      }
+                , new PageRequest(0, 10));
+        System.out.println("asd");
+
     }
 
 }
